a0a0d97e31c218faf9ef476d74c5642bf68db8ea
gopi <gopi@gopi-pc>
2016-09-30 09:53:15 +0530

    V6
    - Compatibility update with shell v 3.22
    - Gnome style object bindings, removal of _this local object

--- refresh-wifi@kgshank.net/extension.js
+++ refresh-wifi@kgshank.net/extension.js
@@ -21,33 +21,31 @@ const WirelessDialog = imports.ui.status.network.NMWirelessDialog
 function init() {
 }
 
-function scanCompleted() {
-    let accessPoints = _this._device.get_access_points() || [ ];
-    accessPoints.forEach(Lang.bind(_this, function(ap) {
-	                  _this._accessPointAdded(_this._device, ap);
+function _scanCompleted() {
+    let accessPoints = this._device.get_access_points() || [ ];
+    accessPoints.forEach(Lang.bind(this, function(ap) {
+	                  this._accessPointAdded(this._device, ap);
 	                }));
-    _this._activeApChanged();
-    _this._updateSensitivity();
-    _this._syncView();
+    this._activeApChanged();
+    this._updateSensitivity();
+    this._syncView();
 }
 
-var _this;
 function _buildLayoutLocal() {
     this._buildLayoutSuper();
-    _this = this;
     let refreshButton = new St.Button({ reactive: true,
                                        can_focus: true,
                                        track_hover: true,
                                        accessible_name: _("Refresh Connections"),
                                        style_class: 'system-menu-action' });
     refreshButton.child = new St.Icon({ icon_name: "view-refresh-symbolic" });
-    refreshButton.connect('clicked', function(){
-                    let accessPoints = _this._device.get_access_points() || [ ];
-                    accessPoints.forEach(Lang.bind(_this, function(ap) {
-                        _this._accessPointRemoved(_this._device, ap);
+    refreshButton.connect('clicked', Lang.bind(this, function(){
+                    let accessPoints = this._device.get_access_points() || [ ];
+                    accessPoints.forEach(Lang.bind(this, function(ap) {
+                    	this._accessPointRemoved(this._device, ap);
                     }));
-                    _this._device.request_scan_simple(scanCompleted);                 
-                });
+                    this._device.request_scan_simple(Lang.bind(this,this._scanCompleted));                 
+                }));
     this.contentLayout.first_child.add(refreshButton ,{ expand: true,
                                                        x_fill: false,
                                                        x_align: St.Align.END });
@@ -57,6 +55,7 @@ function enable() {
     if(!WirelessDialog.prototype._buildLayoutSuper) {
         WirelessDialog.prototype._buildLayoutSuper = WirelessDialog.prototype._buildLayout;
         WirelessDialog.prototype._buildLayout = _buildLayoutLocal;
+        WirelessDialog.prototype._scanCompleted = _scanCompleted
     }
 }
 
@@ -64,9 +63,7 @@ function disable() {
     if(WirelessDialog.prototype._buildLayoutSuper) {
         WirelessDialog.prototype._buildLayout = WirelessDialog.prototype._buildLayoutSuper;
         delete WirelessDialog.prototype['_buildLayoutSuper'];
-    }
-    if(_this) {
-        _this = null;
+        delete WirelessDialog.prototype['_scanCompleted'];
     }    
 }
 
